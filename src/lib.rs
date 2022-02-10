8b753d9501f298664c495770cf9079d21ac2c8ed
Josh Jones <ohblonddev@gmail.com>
2022-02-10 20:42:47 +0000

    Prioritise height

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-#[derive(derive_new::new, derive_getters::Getters, Clone)]
+#[derive(derive_new::new, derive_getters::Getters, Clone, Debug)]
 struct Fruit {
     species: Species,
+    height: usize,
 }
 
 #[derive(Eq, PartialEq, Debug, Copy, Clone)]
@@ -14,7 +15,10 @@ struct FruitPrioritisationService;
 
 impl FruitPrioritisationService {
     fn prioritise(&self, fruit: Vec<Fruit>) -> Fruit {
-        prioritise_species(fruit).first().unwrap().clone()
+        let species_prioritised = prioritise_species(fruit);
+        let height_prioritised = prioritise_height(species_prioritised);
+
+        height_prioritised.first().unwrap().clone()
     }
 }
 
@@ -37,6 +41,11 @@ fn prioritise_species(mut input: Vec<Fruit>) -> Vec<Fruit> {
     input
 }
 
+fn prioritise_height(mut input: Vec<Fruit>) -> Vec<Fruit> {
+    input.sort_by(|a, b| b.height().cmp(a.height()));
+    input
+}
+
 #[cfg(test)]
 mod tests {
     use speculoos::prelude::*;
@@ -46,12 +55,20 @@ mod tests {
     #[test]
     fn test() {
         let under_test = FruitPrioritisationService::new();
-        let mut fruit_input: Vec<Fruit> =
-            vec![Fruit::new(Species::Apple), Fruit::new(Species::Pear)];
+        let mut fruit_input: Vec<Fruit> = vec![
+            Fruit::new(Species::Apple, 1),
+            Fruit::new(Species::Apple, 2),
+            Fruit::new(Species::Pear, 1),
+            Fruit::new(Species::Pear, 2),
+        ];
 
-        assert_that(&under_test.prioritise(fruit_input.clone()).species())
-            .is_equal_to(&Species::Apple);
+        let selected = under_test.prioritise(fruit_input.clone());
+        assert_that(&selected.species()).is_equal_to(&Species::Apple);
+        assert_that(&selected.height()).is_equal_to(&2);
+
         fruit_input.reverse();
-        assert_that(&under_test.prioritise(fruit_input).species()).is_equal_to(&Species::Apple);
+        let selected_reverse = under_test.prioritise(fruit_input);
+        assert_that(&selected_reverse.species()).is_equal_to(&Species::Apple);
+        assert_that(&selected_reverse.height()).is_equal_to(&2);
     }
 }
