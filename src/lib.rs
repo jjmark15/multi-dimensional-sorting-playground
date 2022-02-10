8daed34d19994e1009eab1e8ea4d245092b5ecdd
Josh Jones <ohblonddev@gmail.com>
2022-02-10 20:25:59 +0000

    Prioritise species

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,57 @@
+#[derive(derive_new::new, derive_getters::Getters, Clone)]
+struct Fruit {
+    species: Species,
+}
+
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+enum Species {
+    Apple,
+    Pear,
+}
+
+#[derive(derive_new::new)]
+struct FruitPrioritisationService;
+
+impl FruitPrioritisationService {
+    fn prioritise(&self, fruit: Vec<Fruit>) -> Fruit {
+        prioritise_species(fruit).first().unwrap().clone()
+    }
+}
+
+fn prioritise_species(mut input: Vec<Fruit>) -> Vec<Fruit> {
+    input.sort_by(|a, b| {
+        const ORDER: [Species; 2] = [Species::Apple, Species::Pear];
+
+        let a_index = ORDER
+            .iter()
+            .position(|species| species == a.species())
+            .unwrap();
+        let b_index = ORDER
+            .iter()
+            .position(|species| species == b.species())
+            .unwrap();
+
+        a_index.cmp(&b_index)
+    });
+
+    input
+}
+
 #[cfg(test)]
 mod tests {
+    use speculoos::prelude::*;
+
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test() {
+        let under_test = FruitPrioritisationService::new();
+        let mut fruit_input: Vec<Fruit> =
+            vec![Fruit::new(Species::Apple), Fruit::new(Species::Pear)];
+
+        assert_that(&under_test.prioritise(fruit_input.clone()).species())
+            .is_equal_to(&Species::Apple);
+        fruit_input.reverse();
+        assert_that(&under_test.prioritise(fruit_input).species()).is_equal_to(&Species::Apple);
     }
 }
